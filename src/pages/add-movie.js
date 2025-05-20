a2c3cdf847fd4fd82544158c3a49a5aa4ea0c23f
Spartan Warrior <spartanwarrior@173-16-3-16.client.mchsi.com>
2025-05-20 10:34:56 +0800

    a

--- src/pages/add-movie.js
+++ src/pages/add-movie.js
@@ -1,77 +1,19 @@
-import React, { useState } from 'react';
-
-const AddMoviePage = () => {
-    const [movies, setMovies] = useState([]);
-
-    const [form, setForm] = useState({
+const handleAddMovie = () => {
+  fetch('http://localhost:5000/api/movies', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(form),
+  })
+    .then(res => res.json())
+    .then(newMovie => {
+      setMovies([newMovie, ...movies]);
+      setForm({
         title: '',
         director: '',
         year: '',
         genre: '',
         description: '',
         imageUrl: '',
+      });
     });
-
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
-
-    const handleAddMovie = () => {
-        const { title, director, year, genre, description, imageUrl } = form;
-        if (title && director && year && genre && description && imageUrl) {
-            const newMovie = {
-                id: Date.now(),
-                ...form,
-            };
-            setMovies([newMovie, ...movies]);
-            setForm({
-                title: '',
-                director: '',
-                year: '',
-                genre: '',
-                description: '',
-                imageUrl: '',
-            });
-        }
-    };
-
-    return (
-        <div id='thread-page-container'>
-            <div>
-                <div id='add-movie-title'>Add New Movie</div>
-                <div id='movie-title-ha'>
-                    Movie Title
-                </div>
-                <input type="text" name="title" value={form.title} onChange={handleChange} id='input-movie-title'/>
-                <div id='movie-title-ha'>
-                    Director
-                </div>
-                <input type="text" name='director' value={form.director} onChange={handleChange} id='input-movie-title'/>
-                <div id='movie-title-ha'>
-                    Release Year
-                </div>
-                <input type='number' name='year' id='input-movie-title' value={form.year} onChange={handleChange}/>
-                <div id='movie-title-ha'>
-                    Genre
-                </div>
-                <input type='text' name='genre' id='input-movie-title' value={form.genre} onChange={handleChange}/>
-                <div id='movie-title-ha'>
-                    Description
-                </div>
-                <input type='text' name='description' id='input-movie-title' value={form.description} onChange={handleChange}/>
-                <div id='movie-title-ha'>
-                    Image URL
-                </div>
-                <input type='text' name='imageUrl' id='input-movie-title' value={form.imageUrl} onChange={handleChange}/>
-
-                <hr />
-
-                <button id='add-movie-button' onClick={handleAddMovie}>
-                    Add Movie
-                </button>
-            </div>
-        </div>
-    );
 };
-
-export default AddMoviePage;
--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,66 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MovieCard from '../components/movie-card';
 
 const MoviesPage = () => {
-    const [movies] = useState([
-        {
-            id: 1,
-            title: 'The Shawshank Redemption',
-            director: 'Frank Darabont',
-            genre: 'Drama',
-            imageUrl: '',
-        },
-        {
-            id: 2,
-            title: 'Inception',
-            director: 'Christopher Nolan',
-            genre: 'Sci-Fi',
-            imageUrl: '',
-        },
-        {
-            id: 3,
-            title: 'The Godfather',
-            director: 'Francis Ford Coppola',
-            genre: 'Crime',
-            imageUrl: '',
-        },
-        {
-            id: 4,
-            title: 'The Matrix',
-            director: 'The Wachowskis',
-            genre: 'Action',
-            imageUrl: '',
-        },
-    ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/movies')
+      .then(res => res.json())
+      .then(data => setMovies(data));
+  }, []);
 
   return (
-    <div id="home-page">
-      <div id="movie-page-top">
-        <div id="movie-searching">
-          <div>
-            <input id="movie-input" />
-          </div>
-          <div id="search-movie-button-container">
-            <button id="search-movie-button">Search</button>
-          </div>
-        </div>
-        <div id="filter-genre">Filter By Genre</div>
-        <div id="genres">
-          <button id="all">All</button>
-          <button id="all">Action</button>
-          <button id="all">Crime</button>
-          <button id="all">Sci-Fi</button>
-          <button id="all">Drama</button>
-        </div>
-      </div>
-
-      <div id="movie-page-bottom">
-        <div id="movies-1">
-          {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))}
-        </div>
-      </div>
+    <div>
+      {movies.map(movie => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 };
